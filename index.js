bdaccee1b40c10f1c66ea93fdf7e983868c8827a
Christopher Lis <christopher4lis@gmail.com>
2022-10-24 19:13:09 -0400

    Gravity

--- index.js
+++ index.js
@@ -4,5 +4,47 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+const gravity = 0.5
+
+class Player {
+  constructor(position) {
+    this.position = position
+    this.velocity = {
+      x: 0,
+      y: 1,
+    }
+    this.height = 100
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 100, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.y += this.velocity.y
+    if (this.position.y + this.height + this.velocity.y < canvas.height)
+      this.velocity.y += gravity
+    else this.velocity.y = 0
+  }
+}
+
+const player = new Player({
+  x: 0,
+  y: 0,
+})
+const player2 = new Player({
+  x: 300,
+  y: 100,
+})
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  player2.update()
+}
+
+animate()
