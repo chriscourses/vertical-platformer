4823b8ca38666f6ef9537e2c55615fbf2e378af3
Christopher Lis <christopher4lis@gmail.com>
2022-10-24 22:20:34 -0400

    Background sprite

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+const scaledCanvas = {
+  width: canvas.width / 4,
+  height: canvas.height / 4,
+}
+
 const gravity = 0.5
+
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position
+    this.image = new Image()
+    this.image.src = imageSrc
+  }
+
+  draw() {
+    if (!this.image) return
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+
+  update() {
+    this.draw()
+  }
+}
 
 class Player {
   constructor(position) {
@@ -51,10 +73,25 @@ const keys = {
   },
 }
 
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: './img/background.png',
+})
+
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'white'
   c.fillRect(0, 0, canvas.width, canvas.height)
+
+  c.save()
+  c.scale(4, 4)
+  c.translate(0, -background.image.height + scaledCanvas.height)
+  background.update()
+  c.restore()
+
   player.update()
   player2.update()
 
